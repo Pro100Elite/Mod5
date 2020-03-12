38b24a811b1dd9cdf470175bd4043fa4b8db460a
Yevhenii <pro100elite@icloud.com>
2020-03-12 22:16:55 +0200

    first

---
+++ MyWebApp1/MyWebApp1/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MyWebApp1.Startup))]
+namespace MyWebApp1
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
