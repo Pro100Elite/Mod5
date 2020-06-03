15c10aecab480680a13291a236801540630f213e
Yevhenii <pro100elite@icloud.com>
2020-06-04 01:06:14 +0300

    HeaderFix

---
+++ PortalForReading/PortalForReading/Common/MyFormatAttribute.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace PortalForReading.Common
+{
+    public class MyFormatAttribute : ValidationAttribute
+    {
+        private static string[] myFormat;
+
+        public MyFormatAttribute(string[] Formats)
+        {
+            myFormat = Formats;
+        }
+
+        public override bool IsValid(object value)
+        {
+            HttpPostedFileBase f = value as HttpPostedFileBase;
+
+            if (f != null)
+            {
+                string strval = f.ContentType;
+                for (int i = 0; i < myFormat.Length; i++)
+                {
+                    if (strval == myFormat[i])
+                        return true;
+                }
+            }
+
+            return false;
+        }
+    }
+}
